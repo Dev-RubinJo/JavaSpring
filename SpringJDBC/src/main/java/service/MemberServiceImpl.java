81d3a5e9a4bc49d8dc07ce5105d76024dbb375ed
Rubin.Jo <dbqlsdl1234@naver.com>
2021-02-22 09:32:23 +0900

    Spring에서 JDBC를 이용하는 방법에 대해 공부 후 커밋

---
+++ SpringJDBC/src/main/java/service/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import dao.MemberDAO;
+
+public class MemberServiceImpl implements MemberService {
+    private MemberDAO memberDAO;
+
+    public void setMemberDAO(MemberDAO memberDAO) {
+        this.memberDAO = memberDAO;
+    }
+
+    @Override
+    public List listMembers() throws DataAccessException {
+        List membersList = null;
+        membersList = memberDAO.selectAllMembers();
+        return membersList;
+    }
+}
